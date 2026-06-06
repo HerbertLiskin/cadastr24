18c5e04919f483725e0d4887d756498b9af6fbb3
HerbertLiskin <gkljatsko@gmail.com>
2026-06-06 11:51:55 +0300

    Update VotingResults.tsx

--- src/widgets/voting-results/VotingResults.tsx
+++ src/widgets/voting-results/VotingResults.tsx
@@ -12,11 +12,11 @@ interface VotingResultsProps {
 export const VotingResults: React.FC<VotingResultsProps> = ({ apartments, totalArea }) => {
   const navigate = useNavigate();
 
-  const elecFull = [4, 10, 11, 16, 20, 21, 24, 36, 39, 48, 53, 54, 55, 56, 58, 60, 62, 65, 73, 74, 76, 77, 78];
-  const paperFull = [2, 17, 22, 27, 28, 30, 38, 42, 43, 45, 47, 50, 52, 63, 68, 75];
+  const elecFull = [4, 10, 11, 16, 20, 21, 24, 36, 39, 48, 53, 54, 55, 56, 58, 60, 62, 65, 73, 74, 75, 76, 77, 78];
+  const paperFull = [2, 17, 22, 27, 28, 30, 38, 42, 43, 45, 47, 50, 52, 59, 63, 68];
   const cityFull = [1, 5];
   const planningFull: number[] = [];
-  const refusedFull = [9, 14, 18, 25, 29, 41, 44, 59];
+  const refusedFull = [9, 14, 18, 25, 29, 41, 44];
 
   // Helper structures to display voters in order
   const elecVoters = useMemo(() => {
